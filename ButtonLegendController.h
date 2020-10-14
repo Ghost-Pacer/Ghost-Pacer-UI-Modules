7c0294f2ae300aeb65bee9ca507ed9bb71846e76
RonithK <ronithkandallu@gmail.com>
2020-10-14 15:57:40 -0700

    Converted files from using tabs to spaces

--- ButtonLegendController.h
+++ ButtonLegendController.h
@@ -5,36 +5,36 @@
 
 class ButtonLegendController {
 public:
-	// ButtonLegendController Constructor
-	ButtonLegendController(int spacing, CPVRTPrint3D *print3D);
+    // ButtonLegendController Constructor
+    ButtonLegendController(int spacing, CPVRTPrint3D *print3D);
 
-	// ButtonLegendController Destructor
-	~ButtonLegendController();
+    // ButtonLegendController Destructor
+    ~ButtonLegendController();
 
-	// Renders button legend on screen
-	void render();
+    // Renders button legend on screen
+    void render();
 
-	// Sets the button at the given index with either its
-	// selected or deselected color
-	void setButtonIsEnabled(int buttonIndex, bool enabled);
+    // Sets the button at the given index with either its
+    // selected or deselected color
+    void setButtonIsEnabled(int buttonIndex, bool enabled);
 private:
-	const char *UP_BUTTON = "~";
-	const char *DOWN_BUTTON = "`";
-	const char *OK_BUTTON = "\\";
-	const unsigned int UP_BUTTON_SELECTED_COLOR = 0xFFFF0000;
-	const unsigned int DOWN_BUTTON_SELECTED_COLOR = 0xFFFF0000;
-	const unsigned int OK_BUTTON_SELECTED_COLOR = 0xFF00FF00;
-	const unsigned int UNSELECTED_COLOR = 0x99999999;
-	const float SCALE = 0.9;
-	const float X_RELATIVE_CENTER_COORDINATE = 90.0;
-	const float Y_RELATIVE_CENTER_COORDINATE = 88.0;
+    const char *UP_BUTTON = "~";
+    const char *DOWN_BUTTON = "`";
+    const char *OK_BUTTON = "\\";
+    const unsigned int UP_BUTTON_SELECTED_COLOR = 0xFFFF0000;
+    const unsigned int DOWN_BUTTON_SELECTED_COLOR = 0xFFFF0000;
+    const unsigned int OK_BUTTON_SELECTED_COLOR = 0xFF00FF00;
+    const unsigned int UNSELECTED_COLOR = 0x99999999;
+    const float SCALE = 0.9;
+    const float X_RELATIVE_CENTER_COORDINATE = 90.0;
+    const float Y_RELATIVE_CENTER_COORDINATE = 88.0;
 
-	vector<Print2D::AttributedText> legendItemsAttributedText;
-	int itemSpacing;
-	Print2D *print2D;
+    vector<Print2D::AttributedText> legendItemsAttributedText;
+    int itemSpacing;
+    Print2D *print2D;
 
-	char* stringRepresentationOfButton(int buttonIndex);
-	unsigned int selectedColorOfButton(int buttonIndex);
+    char* stringRepresentationOfButton(int buttonIndex);
+    unsigned int selectedColorOfButton(int buttonIndex);
 };
 
 #endif
