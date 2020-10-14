0a7ebcb01dc4a50847c1a328c0ec7aa11bea603b
RonithK <ronithkandallu@gmail.com>
2020-10-13 21:18:47 -0700

    Created ButtonLegendController .cpp & .h files

---
+++ ButtonLegendController.cpp
@@ -0,0 +1,1 @@
+#include "ButtonLegendController.h"
---
+++ ButtonLegendController.h
@@ -0,0 +1,10 @@
+#ifndef __BUTTON_LEGEND_CONTROLLER_H_
+#define __BUTTON_LEGEND_CONTROLLER_H_
+
+#include "Print2D.h"
+
+class ButtonLegendController {
+
+};
+
+#endif
