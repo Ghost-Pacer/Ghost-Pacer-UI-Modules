dcefbb9027d785995e7bc760224ac14b561b96f8
RonithK <ronithkandallu@gmail.com>
2020-10-13 21:55:45 -0700

    Wrote ButtonLegendController interface

--- ButtonLegendController.h
+++ ButtonLegendController.h
@@ -4,7 +4,23 @@
 #include "Print2D.h"
 
 class ButtonLegendController {
+public:
+	ButtonLegendController(int spacing, float scale, CPVRTPrint3D *print3D);
+	~ButtonLegendController();
+	void render();
+	void setButtonIsEnabled(int buttonIndex, bool enabled);
+private:
+	const char UP_BUTTON = '\"';
+	const char DOWN_BUTTON = '#';
+	const char OK_BUTTON = '$';
+	const unsigned int UP_BUTTON_SELECTED_COLOR = 0xFFFF0000;
+	const unsigned int DOWN_BUTTON_SELECTED_COLOR = 0xFFFF0000;
+	const unsigned int OK_BUTTON_SELECTED_COLOR = 0xFF00FF00;
+	const unsigned int UNSELECTED_COLOR = 0x99999999;
 
+	vector<Print2D::AttributedText> legendItemsAttributedText;
+	int itemSpacing;
+	Print2D *print2D;
 };
 
 #endif
