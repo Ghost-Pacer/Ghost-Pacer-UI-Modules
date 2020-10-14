6899fc5cafb1992f8548109015d9d207ae3f3404
RonithK <ronithkandallu@gmail.com>
2020-10-13 22:41:11 -0700

    Button legend can be rendered

--- ButtonLegendController.cpp
+++ ButtonLegendController.cpp
@@ -1,2 +1,40 @@
 #include "ButtonLegendController.h"
+#include <iostream>
 
+ButtonLegendController::ButtonLegendController(int spacing, float scale, CPVRTPrint3D *print3D) {
+	const int BUTTON_COUNT = 3;
+	for (int ii = 0; ii < BUTTON_COUNT; ii++) {
+		Print2D::AttributedText attributedText;
+		attributedText.color = UNSELECTED_COLOR;
+		attributedText.scale = scale;
+		attributedText.text = stringRepresentationOfButton(ii);
+		legendItemsAttributedText.push_back(attributedText);
+	}
+
+	itemSpacing = spacing;
+	print2D = new Print2D(print3D);
+}
+
+ButtonLegendController::~ButtonLegendController() {
+	delete print2D;
+}
+
+void ButtonLegendController::render() {
+	const float X_RELATIVE_CENTER_COORDINATE = 85.0;
+	const float Y_RELATIVE_CENTER_COORDINATE = 85.0;
+	print2D->renderVerticalMenuCenteredAt(X_RELATIVE_CENTER_COORDINATE, Y_RELATIVE_CENTER_COORDINATE, itemSpacing, legendItemsAttributedText);
+}
+
+// ***** HELPER FUNCTIONS ****
+char* ButtonLegendController::stringRepresentationOfButton(int buttonIndex) {
+	switch (buttonIndex) {
+	case 0:
+		return const_cast<char*>(UP_BUTTON);
+	case 1:
+		return const_cast<char*>(DOWN_BUTTON);
+	case 2:
+		return const_cast<char*>(OK_BUTTON);
+	default:
+		return "\0";
+	}
+}
--- ButtonLegendController.h
+++ ButtonLegendController.h
@@ -10,9 +10,9 @@ public:
 	void render();
 	void setButtonIsEnabled(int buttonIndex, bool enabled);
 private:
-	const char UP_BUTTON = '\"';
-	const char DOWN_BUTTON = '#';
-	const char OK_BUTTON = '$';
+	const char *UP_BUTTON = "\"";
+	const char *DOWN_BUTTON = "#";
+	const char *OK_BUTTON = "$";
 	const unsigned int UP_BUTTON_SELECTED_COLOR = 0xFFFF0000;
 	const unsigned int DOWN_BUTTON_SELECTED_COLOR = 0xFFFF0000;
 	const unsigned int OK_BUTTON_SELECTED_COLOR = 0xFF00FF00;
@@ -21,6 +21,8 @@ private:
 	vector<Print2D::AttributedText> legendItemsAttributedText;
 	int itemSpacing;
 	Print2D *print2D;
+
+	char* stringRepresentationOfButton(int buttonIndex);
 };
 
 #endif
